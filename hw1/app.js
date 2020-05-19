26473a5cfe0c32b0afc913c7032d00a2ec768789
MariaNegrych <negrychmariya@gmail.com>
2020-05-19 12:47:09 +0300

    hw1 done

--- hw1/app.js
+++ hw1/app.js
@@ -11,60 +11,56 @@ const path = require('path');
 //     err => {
 //     console.log(err);
 // });
-
+//
 // fs.writeFile(path.join(__dirname, 'first-group', 'vasia.txt'),
 //     'name: Vasia, \n age: 23',
 //     err => {
 //         console.log(err);
 //     });
-
+//
 // fs.mkdir(path.join(__dirname, 'second-group'),
 //     err => {
 //         console.log(err);
 //     });
-
+//
 // fs.writeFile(path.join(__dirname, 'second-group', 'nastia.txt'),
 //     'name: Nastia, \n age: 25',
 //     err => {
 //         console.log(err);
 //     });
-
+//
 // fs.writeFile(path.join(__dirname, 'second-group', 'valia.txt'),
 //     'name: Valia, \n age: 24',
 //     err => {
 //         console.log(err);
 //     });
 
-function fileChanger(firstGroup, secondGroup) {
-    fs.readdir(path.join(__dirname, firstGroup), (err, filesOfFirstGroup) => {
+function fileChanger(fromDir, toDir) {
+    fs.readdir(fromDir, (err, files) => {
         if (err) {
             console.log(err);
         } else {
-            console.log(filesOfFirstGroup);
-            fs.readdir(path.join(__dirname, secondGroup), (err, filesOfSecondGroup) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(filesOfSecondGroup);
-                    for (const files of filesOfFirstGroup) {
-                        fs.rename(path.join(__dirname, firstGroup, files), path.join(__dirname, secondGroup, files),
-                            err => {
-                                console.log(err);
-                            });
+            console.log(files);
+            for (const file of files) {
+                fs.rename(path.join(fromDir, file), path.join(toDir, file), err => {
+                    if (err) {
+                        console.log(err);
                     }
-                    for (const files of filesOfSecondGroup) {
-                        fs.rename(path.join(__dirname, secondGroup, files), path.join(__dirname, firstGroup, files),
-                            err => {
-                                console.log(err);
-                            });
-                    }
-                }
-            })
+                });
+            }
         }
-    })
+    });
 }
 
-fileChanger('first-group', 'second-group');
+function resolver(firstDir, secondDir) {
+    fileChanger(firstDir, secondDir);
+    fileChanger(secondDir, firstDir);
+}
+
+const firstDir = path.join(__dirname,'first-group');
+const secondDir = path.join(__dirname,'second-group');
+
+resolver(firstDir, secondDir);
 
 // fs.readdir(path.join(__dirname, 'first-group'), (err, files) => {
 //     console.log(files);
